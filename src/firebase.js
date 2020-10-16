d4e5c0e3e924ef1f7cfb6020f18a2c1ea4c7ff52
OlawaleJoseph <adedekojoseph@gmail.com>
2020-10-16 23:16:14 +0100

    clean up codebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default  {
+  apiKey: "AIzaSyACwxj5EErmCNrGTPPnh2fETaqJ_iRS_jQ",
+  authDomain: "clone-2da1e.firebaseapp.com",
+  databaseURL: "https://clone-2da1e.firebaseio.com",
+  projectId: "clone-2da1e",
+  storageBucket: "clone-2da1e.appspot.com",
+  messagingSenderId: "728583253188",
+  appId: "1:728583253188:web:da4313593d124f3f51ed91",
+  measurementId: "G-KS9L6CRC3D"
+};
